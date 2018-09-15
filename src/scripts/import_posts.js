83e28693940e50432f04f79ff18bea830859f3f7
Joaquin Bravo Contreras <jackbravo@gmail.com>
2018-09-14 22:29:35 -0500

    Make nested queries actually run (fix db handling error)

--- src/scripts/import_posts.js
+++ src/scripts/import_posts.js
@@ -12,41 +12,42 @@ let db = new sqlite3.Database(process.argv[2], sqlite3.OPEN_READONLY, (err) => {
   }
 });
 
-db.serialize(() => {
-  db.each('SELECT n.nid, n.title, n.created, b.body_value FROM node n ' +
-    'INNER JOIN field_data_body b ON b.entity_id = n.nid LIMIT 4', (err, row) => {
+db.each('SELECT n.nid, n.title, n.created, b.body_value FROM node n ' +
+  'INNER JOIN field_data_body b ON b.entity_id = n.nid LIMIT 4', (err, row) => {
+  if (err) {
+    console.error('Error at each SELECT nid', err.message);
+  }
+  const slug = slugify(row.title);
+  const path = __dirname + '/../pages/' + slug;
+  const date = new Date(row.created * 1000);
+  let aliases = [];
+  db.each('SELECT alias FROM url_alias WHERE source = \'node/' + row.nid + '\'', (err, row) => {
+    if (err) {
+      console.error('Error at each SELECT alias', err.message);
+      return;
+    }
+    aliases.push(row.alias);
+  }, (err, count) => {
+    if (err) {
+      console.error('Error at complete SELECT alias', err.message);
+      return;
+    }
+    fs.mkdir(path, (err) => {
       if (err) {
-        console.error(err.message);
+        console.log(path, ' already exists');
       }
-      const slug = slugify(row.title);
-      const path = __dirname + '/../pages/' + slug;
-      const date = new Date(row.created * 1000);
-      let aliases = [];
-      db.each('SELECT alias FROM url_alias WHERE source = \'node/' + row.nid + '\'', (err, row) => {
-          if (err) {
-            console.error(err.message);
-            return;
-          }
-          aliases.push(row.alias);
-        })
-      fs.mkdir(path, (err) => {
-        if (err) {
-          console.log(path, ' already exists');
-          const file = fs.createWriteStream(path + '/index.md', { flags: 'w' });
-          file.write('---\n');
-          file.write('title: ' + row.title + '\n');
-          file.write('date: "' + date.toISOString() + '"\n');
-          file.write('aliases: ' + JSON.stringify(aliases) + '\n');
-          file.write('---\n\n');
-          file.write(row.body_value);
-          file.end();
-        }
-      });
-      console.log(aliases, date, slugify(row.title));
+      const file = fs.createWriteStream(path + '/index.md', { flags: 'w' });
+      file.write('---\n');
+      file.write('title: ' + row.title + '\n');
+      file.write('date: "' + date.toISOString() + '"\n');
+      file.write('aliases: ' + JSON.stringify(aliases) + '\n');
+      file.write('---\n\n');
+      file.write(row.body_value);
+      file.end();
     });
+    console.log(aliases, date, slugify(row.title));
+  })
 });
-
-db.close();
 
 function usage() {
   const path = require('path');
